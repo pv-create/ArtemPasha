0dd223e1dc94f2de368c35ae30820d34efc04fd7
Павел Вилков <pavel_vilkov17@mail.ru>
2022-01-18 07:27:31 +0700

    add navbar and footer and still need to refactor it

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,49 @@ function App() {
 
   return (
     <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider"/>
+                  </li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#"  aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
       <div className="d-flex flex-column flex-shrink-0 bg-dark" style={{width: "4.5rem"}}>
 
         <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
@@ -64,9 +107,140 @@ function App() {
               </svg>
             </a>
           </li>
+          <li>
+            <a href="#" className="nav-link py-3 border-bottom" title="Customers" data-bs-toggle="tooltip"
+               data-bs-placement="right">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                   className="bi bi-cursor-fill" viewBox="0 0 16 16">
+                <path
+                    d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link py-3 border-bottom" title="Customers" data-bs-toggle="tooltip"
+               data-bs-placement="right">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                   className="bi bi-cursor-fill" viewBox="0 0 16 16">
+                <path
+                    d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
+              </svg>
+            </a>
+          </li>
         </ul>
 
       </div>
+      <footer className="text-center text-lg-start bg-dark text-muted">
+
+        <section
+            className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+        >
+
+          <div className="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+          </div>
+
+          <div>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-google"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+
+        </section>
+
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+
+            <div className="row mt-3">
+
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                <h6 className="text-uppercase fw-bold mb-4">
+                  <i className="fas fa-gem me-3"></i>Company name
+                </h6>
+                <p>
+                  Here you can use rows and columns to organize your footer content. Lorem ipsum
+                  dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 className="text-uppercase fw-bold mb-4">
+                  Products
+                </h6>
+                <p>
+                  <a href="#!" className="text-reset">Angular</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">React</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Vue</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Laravel</a>
+                </p>
+              </div>
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 className="text-uppercase fw-bold mb-4">
+                  Useful links
+                </h6>
+                <p>
+                  <a href="#!" className="text-reset">Pricing</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Settings</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Orders</a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">Help</a>
+                </p>
+              </div>
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                <h6 className="text-uppercase fw-bold mb-4">
+                  Contact
+                </h6>
+                <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                <p>
+                  <i className="fas fa-envelope me-3"></i>
+                  info@example.com
+                </p>
+                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+        <div className="text-center p-4">
+          © 2021 Copyright:
+          <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+      </footer>
 
     </div>
 
